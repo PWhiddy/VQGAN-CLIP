eaabebcb59204353717e86c41d1098925e35c938
PWhiddy <all.cows.like.to.moo@gmail.com>
2021-10-06 06:03:41 +0000

    update install

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@ apt update
 apt install -y vim tmux htop curl ffmpeg
 pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r requirements.txt
+pip install ffmpeg-python
 pip install ftfy regex tqdm omegaconf pytorch-lightning IPython kornia imageio imageio-ffmpeg einops torch_optimizer
-git clone 'https://github.com/nerdyrodent/VQGAN-CLIP'
-cd VQGAN-CLIP
 git clone 'https://github.com/openai/CLIP'
 git clone 'https://github.com/CompVis/taming-transformers'
 chmod +x download_models.sh
